140e4f3018f067e965ff31a0f829bd5a2e2566b5
Alexandre Macé <34915784+alexandre-mace@users.noreply.github.com>
2022-07-18 12:14:02 +0200

    Update questions.js

--- data/questions.js
+++ data/questions.js
@@ -19,11 +19,13 @@ const questions = [
         ]
     },
     {
-        title: "2",
+        title: "Est-il trop tard pour agir ?",
         category: categories.one,
-        answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias assumenda, cumque debitis doloremque dolores facere iste iure modi, nostrum, obcaecati pariatur reprehenderit tenetur. Accusamus amet commodi esse est eum, nihil.\n",
+        answer: "Non, il n'est pas trop tard pour agir sur le réchauffement planétaire : \"le réchauffement devrait plus ou moins s’arrêter lorsque les émissions de dioxyde de carbone (CO2) seront nulles, ce qui signifie que l’Homme a le pouvoir de choisir son avenir climatique\" \n",
         sources: [
-            'https://bonpote.com/synthese-et-analyse-du-nouveau-rapport-du-giec/'
+            'https://www.youtube.com/watch?v=iiKuUElvGhM',
+            'https://www.carbonbrief.org/explainer-will-global-warming-stop-as-soon-as-net-zero-emissions-are-reached/',
+            'https://bonpote.com/existe-t-il-vraiment-une-inertie-climatique-de-20-ans/'
         ]
     },
     {
